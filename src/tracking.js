6a83c0ad0ccb3120bc6e6666ddedd62354c24482
Josh Robbs <joshrobbs@Zuul.local>
2018-09-25 13:41:34 -0600

    updating navigator

--- src/tracking.js
+++ src/tracking.js
@@ -278,7 +278,7 @@ if (!window.URL) {
   window.URL = window.URL || window.webkitURL || window.msURL || window.oURL;
 }
 
-if (!navigator.getUserMedia) {
+if (typeof navigator !== 'undefined' && navigator.getUserMedia) {
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
   navigator.mozGetUserMedia || navigator.msGetUserMedia;
 }
